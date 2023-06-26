7864f08f0f89c32bdda14dc0437dd476f8984320
namefox <thiiagoo912@hotmail.com>
2023-06-26 20:01:06 -0300

    Project sorting

---
+++ Hub/src/com/astrobit/hub/Sorts.java
@@ -0,0 +1,90 @@
+package com.astrobit.hub;
+
+import java.util.Comparator;
+
+public class Sorts {
+
+    public static int MULTIPLIER = -1;
+
+    public static Comparator<Project> defaultComparator() {
+        return (o1, o2) -> 0;
+    }
+
+    public static Comparator<Project> lastModified() {
+        return (o1, o2) -> {
+            long time1 = o1.modified().getTime();
+            long time2 = o2.modified().getTime();
+
+            if (time1 > time2) return MULTIPLIER;
+            if (time1 < time2) return -MULTIPLIER;
+
+            return 0;
+        };
+    }
+
+    public static Comparator<Project> alphabetical() {
+        return (o1, o2) -> {
+            String[] split = o1.path().split("/");
+            char[] chars1 = split[split.length - 1].toCharArray();
+
+            split = o2.path().split("/");
+            char[] chars2 = split[split.length - 1].toCharArray();
+
+            for (int i = 0; i < chars1.length; i++) {
+                if (i >= chars2.length) return MULTIPLIER;
+
+                if (chars1[i] == chars2[i]) continue;
+
+                if (chars1[i] > chars2[i]) return -MULTIPLIER;
+                if (chars1[i] < chars2[i]) return MULTIPLIER;
+            }
+
+            return 0;
+        };
+    }
+
+    public static Comparator<String> alphabeticalMap() {
+        return (o1, o2) -> {
+            char[] chars1 = o1.toCharArray();
+            char[] chars2 = o2.toCharArray();
+
+            for (int i = 0; i < chars1.length; i++) {
+                if (i >= chars2.length) return MULTIPLIER;
+
+                if (chars1[i] == chars2[i]) continue;
+
+                if (chars1[i] > chars2[i]) return -MULTIPLIER;
+                if (chars1[i] < chars2[i]) return MULTIPLIER;
+            }
+
+            return 0;
+        };
+    }
+
+    public static Comparator<Project> editorVersion() {
+        return (o1, o2) -> {
+            char[] chars1 = o1.editorVersion().toCharArray();
+            char[] chars2 = o2.editorVersion().toCharArray();
+
+            for (int i = 0; i < chars1.length; i++) {
+                if (i >= chars2.length) return MULTIPLIER;
+
+                if (chars1[i] == chars2[i]) continue;
+
+                if (chars1[i] > chars2[i]) return -MULTIPLIER;
+                if (chars1[i] < chars2[i]) return MULTIPLIER;
+            }
+
+            return 0;
+        };
+    }
+
+    public static Comparator<Project> get(int index) {
+        return switch (index) {
+            case 1 -> alphabetical();
+            case 2 -> lastModified();
+            case 3 -> editorVersion();
+            default -> defaultComparator();
+        };
+    }
+}
--- Hub/src/com/astrobit/hub/pages/ProjectsPage.java
+++ Hub/src/com/astrobit/hub/pages/ProjectsPage.java
@@ -1,10 +1,8 @@
 package com.astrobit.hub.pages;
 
-import com.astrobit.hub.Debug;
-import com.astrobit.hub.HubConfiguration;
+import com.astrobit.hub.*;
 import com.astrobit.hub.windows.Main;
 import com.astrobit.hub.components.Page;
-import com.astrobit.hub.Project;
 import com.astrobit.hub.components.ProjectComponent;
 import com.astrobit.hub.windows.CreateProjectForm;
 
@@ -15,12 +13,13 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.*;
 
 public class ProjectsPage extends Page {
 
-    private ArrayList<Project> projects;
-    private final JPanel list;
+    private HashMap<String, ArrayList<Project>> lists;
+    private final JTabbedPane tabbedPane;
+    private int sortBy, tab;
 
     public ProjectsPage() {
         JPanel top = new JPanel();
@@ -38,19 +37,31 @@ public class ProjectsPage extends Page {
         open.addActionListener(this::open);
         east.add(open);
 
-        JLabel label = new JLabel("Projects");
-        label.setFont(label.getFont().deriveFont(30f).deriveFont(Font.BOLD));
-        label.setHorizontalAlignment(JLabel.CENTER);
+        JComboBox<String> sortBox = new JComboBox<>();
+        sortBox.setModel(new DefaultComboBoxModel<>(new String[] {"Sort by", "Alphabetical Order", "Last Modified", "Editor Version"}));
+        sortBox.addItemListener(e -> {
+            sortBy = sortBox.getSelectedIndex();
+            reset();
+            onShow();
+        });
+        east.add(sortBox);
+
+        JComboBox<String> multiplierBox = new JComboBox<>();
+        multiplierBox.setModel(new DefaultComboBoxModel<>(new String[] {"Lowest to Highest", "Highest to Lowest"}));
+        multiplierBox.addItemListener(e -> {
+            Sorts.MULTIPLIER = multiplierBox.getSelectedIndex() == 0 ? -1 : 1;
+            reset();
+            onShow();
+        });
+        east.add(multiplierBox);
 
         top.add(east, BorderLayout.EAST);
-        top.add(label);
 
         setLayout(new BorderLayout());
         add(top, BorderLayout.NORTH);
 
-        list = new JPanel();
-        list.setLayout(new BoxLayout(list, BoxLayout.Y_AXIS));
-        add(new JScrollPane(list));
+        tabbedPane = new JTabbedPane();
+        add(tabbedPane);
     }
 
     private void open(ActionEvent e) {
@@ -73,22 +84,49 @@ public class ProjectsPage extends Page {
     @Override
     @SuppressWarnings("unchecked")
     public void onShow() {
-        projects = (ArrayList<Project>) HubConfiguration.get("projects", new ArrayList<Project>());
+        lists = (HashMap<String, ArrayList<Project>>) HubConfiguration.get("projects", new HashMap<>());
 
-        for (Project project: projects)
-            list.add(new ProjectComponent(project));
+        JPanel all = new JPanel();
+        all.setLayout(new BoxLayout(all, BoxLayout.Y_AXIS));
+        tabbedPane.addTab("All", new JScrollPane(all));
 
-        list.revalidate();
-        list.repaint();
+        @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+        TreeMap<String, ArrayList<Project>> sort = new TreeMap<>(Sorts.alphabeticalMap());
+        sort.putAll(lists);
+
+        for (Map.Entry<String, ArrayList<Project>> entry: sort.entrySet()) {
+            JPanel tab;
+
+            if (entry.getKey() == null) {
+                tab = all;
+            } else {
+                tab = new JPanel();
+                tab.setLayout(new BoxLayout(tab, BoxLayout.Y_AXIS));
+            }
+
+            ArrayList<Project> sorted = (ArrayList<Project>) entry.getValue().clone();
+            sorted.sort(Sorts.get(sortBy));
+
+            for (Project project: sorted) {
+                tab.add(new ProjectComponent(project));
+                if (!tab.equals(all))
+                    all.add(new ProjectComponent(project));
+            }
+
+            tabbedPane.addTab(entry.getKey(), new JScrollPane(tab));
+        }
+
+        tabbedPane.setSelectedIndex(tab);
+
+        tabbedPane.revalidate();
+        tabbedPane.repaint();
     }
 
     @Override
     public void reset() {
-        HubConfiguration.set("projects", projects);
+        HubConfiguration.set("projects", lists);
 
-        Component[] components = list.getComponents();
-        for (Component component: components)
-            if (component instanceof ProjectComponent)
-                list.remove(component);
+        tab = tabbedPane.getSelectedIndex();
+        tabbedPane.removeAll();
     }
 }
